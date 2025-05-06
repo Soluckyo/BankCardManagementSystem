603f8fc46c51404c8193ac0ac8ba05c14194ffbf
Soluckyo <twinki0000@yandex.ru>
2025-05-06 21:26:24 +0300

    добавил миграции бд с помощью Liquibase

--- src/main/java/org/lib/bankcardmanagementsystem/entity/User.java
+++ src/main/java/org/lib/bankcardmanagementsystem/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -64,11 +65,12 @@ public class User {
      * Роль пользователя.
      */
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
     /**
      * Все карты пользователя.
      */
-    @OneToMany
+    @OneToMany(mappedBy = "ownerUser", fetch = FetchType.LAZY)
     private List<Card> card;
 }
